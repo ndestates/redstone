78486b1f770a3317620de5deb78200973f125693
Nick Dodsley <nick@ndestates.com>
2023-12-08 13:38:57 +0000

    Speaker resource and form moved plus qualification checklist created

--- app/Models/Speaker.php
+++ app/Models/Speaker.php
@@ -12,10 +12,52 @@ class Speaker extends Model
 
     protected $casts = [
         'id' => 'integer',
+        'qualifications' => 'array',
     ];
 
     public function conferences(): BelongsToMany
     {
         return $this->belongsToMany(Conference::class);
     }
+
+    public function getform(): array
+    [
+        TextInput::make('name')
+            ->required()
+            ->maxLength(255),
+        TextInput::make('email')
+            ->email()
+            ->required()
+            ->maxLength(255),
+        Textarea::make('bio')
+            ->required()
+            ->maxLength(65535)
+            ->columnSpanFull(),
+        TextInput::make('twitter_handle')
+            ->required()
+            ->maxLength(255),
+        CheckboxList::make('qualifications')
+        ->columnSpanFull()
+        ->searchable()
+        ->bulkToggleable()
+        ->options([
+                'business-leader' => 'Business Leader',
+                'charisma' => 'Charismatic Speaker',
+                'first-time' => 'First Time Speaker',
+                'hometown-hero' => 'Hometown Hero',
+                'humanitarian' => 'Works in Humanitarian Field',
+                'laracasts-contributor' => 'Laracasts Contributor',
+                'twitter-influencer' => 'Large Twitter Following',
+                'youtube-influencer' => 'Large YouTube Following',
+                'open-source' => 'Open Source Creator / Maintainer',
+                'unique-perspective' => 'Unique Perspective'
+            ]
+        )
+        ->descriptions([
+            'business-leader' => 'Here is a nice long description',
+            'charisma' => 'This is even more information about why you should pick this one',
+        ])
+        ->columns(3),
+];
+    
 }
